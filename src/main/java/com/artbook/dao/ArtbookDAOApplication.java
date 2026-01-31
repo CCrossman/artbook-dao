e163838670c9feecc3abd6ca711f0a913f113459
Chris Crossman <christopher.j.crossman@gmail.com>
2026-01-30 19:27:48 -0700

    INITIAL

---
+++ src/main/java/com/artbook/dao/ArtbookDAOApplication.java
@@ -0,0 +1,13 @@
+package com.artbook.dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtbookDAOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtbookDAOApplication.class, args);
+	}
+
+}
