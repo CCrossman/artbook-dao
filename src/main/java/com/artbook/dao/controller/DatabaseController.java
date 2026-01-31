9a32f5a6f81505df7d07b953f28b1be51cc443c9
Christopher Crossman <CCrossman@users.noreply.github.com>
2026-01-30 20:48:37 -0700

    DataSource autowire (#2)
    - configure Postgres 17 data source in artbook-dao private service
    - run first Flyway migration to create users, roles, and permissions.

--- src/main/java/com/artbook/dao/controller/DatabaseController.java
+++ src/main/java/com/artbook/dao/controller/DatabaseController.java
@@ -2,14 +2,20 @@ package com.artbook.dao.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
 
 @RestController
 @RequestMapping("/api/v1/database")
 public class DatabaseController {
     private static final Logger logger = LoggerFactory.getLogger(DatabaseController.class);
+
+    @Autowired
+    private DataSource dataSource;
 
     @GetMapping
     public void ok() {
